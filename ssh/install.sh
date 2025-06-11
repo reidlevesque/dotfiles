b1ad6fa0e1319801ab72675680055a9c99621840
Reid Levesque <reidlevesque@users.noreply.github.com>
2025-06-11 11:09:16 -0400

    fix: a bug in ssh install.sh

--- ssh/install.sh
+++ ssh/install.sh
@@ -4,14 +4,12 @@ test -d ~/.ssh || {
 	mkdir ~/.ssh
 }
 
-test -L ~/.ssh/config || {
-	ln -fsv "$DOTFILES"/ssh/config ~/.ssh/config
-}
+ln -sfv "$DOTFILES"/ssh/config ~/.ssh/config
 
-test -f "${ICLOUD_CONFIG}/ssh_config" || {
-	touch ~/.ssh/config.private
-}
+# test -f "${ICLOUD_CONFIG}/ssh_config" || {
+# 	touch ~/.ssh/config.private
+# }
 
-test -L ~/.ssh/config.private || {
-	ln -sfv "${ICLOUD_CONFIG}/ssh_config" ~/.ssh/config.private
-}
+# test -L ~/.ssh/config.private || {
+# 	ln -sfv "${ICLOUD_CONFIG}/ssh_config" ~/.ssh/config.private
+# }
