1f3f0aef40aa2d258dd19593364f76de8daac108
Reid Levesque <reidlevesque@users.noreply.github.com>
2026-01-08 20:17:29 -0500

    fix: add mise auth function

--- zsh/mise.zsh
+++ zsh/mise.zsh
@@ -1,3 +1,16 @@
 #! /bin/zsh
 
 eval "$(~/.local/bin/mise activate zsh)"
+
+mise () {
+	if [ -z "${MISE_GITHUB_TOKEN}" ]
+	then
+		export MISE_GITHUB_TOKEN=$(command gh auth token 2>/dev/null)
+	fi
+	if typeset -f _mise_original > /dev/null 2>&1
+	then
+		_mise_original "$@"
+	else
+		command mise "$@"
+	fi
+}
