a7f29e2c38ea1336fe921fc2449e4ef170ad85b0
Reid Levesque <reidlevesque@users.noreply.github.com>
2026-01-19 11:48:28 -0500

    fix: cleanup linux brewfile

--- zsh/vscode.zsh
+++ zsh/vscode.zsh
@@ -2,10 +2,10 @@
 
 if [[ "$(/usr/bin/uname 2>/dev/null || uname)" == "Linux" ]]; then
   function code {
-    local VSCODE_SERVER=$(ls -td $HOME/.cursor-server/cli/servers/Stable-*[0-9] | head -1)
+    CURSOR_CLI=$(ls -td $HOME/.cursor-server/bin/linux-x64/*/bin/remote-cli/cursor | head -1)
     # needed to be able to open files in VS Code's editor from the command line
     export VSCODE_IPC_HOOK_CLI="$( ls -td /run/user/$UID/vscode-ipc-*.sock | head -1)"
 
-    $VSCODE_SERVER/server/bin/remote-cli/cursor $@
+    $CURSOR_CLI $@
   }
 fi
